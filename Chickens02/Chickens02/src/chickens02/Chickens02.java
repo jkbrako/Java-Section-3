0e8fb9351a11dd129f660a037e25f5572e90f56a
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-02-26 12:51:58 -0500

    finished chicken02

--- Chickens02/Chickens02/src/chickens02/Chickens02.java
+++ Chickens02/Chickens02/src/chickens02/Chickens02.java
@@ -3,7 +3,19 @@ package chickens02;
 
 public class Chickens02 {
     public static void main(String[] args) {
-        //Put yout code here
+        double mondayAverage = 100;
+        double tuesdayAverage = 121;
+        double wednesdayAverage = 117;
+        
+        double dailyAverage = 0;
+        dailyAverage = (mondayAverage + tuesdayAverage + wednesdayAverage) / 3;
+        
+        double monthlyAverage = 0;
+        monthlyAverage = dailyAverage * 30;
+        
+        double eggProfit = 0.18;
+        double monthlyProfit = 0;
+        monthlyProfit =  monthlyAverage * eggProfit;
 
         System.out.println("Daily Average:   " +dailyAverage);
         System.out.println("Monthly Average: " +monthlyAverage);
