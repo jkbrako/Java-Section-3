b0120d6065147262d8d1bc9cb17a8f16844352a5
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-02-26 12:34:43 -0500

    done with chicken01

--- Chickens01/Chickens01/src/chickens01/Chickens01.java
+++ Chickens01/Chickens01/src/chickens01/Chickens01.java
@@ -3,7 +3,20 @@ package chickens01;
 
 public class Chickens01 {
     public static void main(String[] args) {
-        //Put yout code here
+        int eggsPerChicken = 4;
+        int chickenCount = 8;
+        int totalEggs = 0;
+        
+        totalEggs = totalEggs + eggsPerChicken * chickenCount;
+        
+        chickenCount = chickenCount + 1;
+        
+        totalEggs = totalEggs + eggsPerChicken * chickenCount;        
+        
+        chickenCount = chickenCount / 2;
+        
+        totalEggs = totalEggs + eggsPerChicken * chickenCount;
+
         
         System.out.println(totalEggs);
     }   
