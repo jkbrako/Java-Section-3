372417348aa73fc3da9ed1a774f2f8b64396bd69
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-02-26 13:11:51 -0500

    finished shoppingCart02

--- ShoppingCart02/ShoppingCart02/src/shoppingcart02/ShoppingCart02.java
+++ ShoppingCart02/ShoppingCart02/src/shoppingcart02/ShoppingCart02.java
@@ -5,18 +5,24 @@ public class ShoppingCart02 {
     public static void main(String[] args) {
         String custName = "Alex";
         String itemDesc = "Shirts";
-        String message = custName+" wants to purchase a "+itemDesc;
+        //String message = custName+" wants to purchase a "+itemDesc;
         
         // Declare and initialize numeric fields: price, tax, quantity.   
-        
-        
+        double price = 25;
+        double tax = 0.78;
+        int quantity = 2;
         
         // Declare and assign a calculated totalPrice
+        int totalPrice = 0;
         
         
         // Modify message to include quantity 
+           String message = custName+" wants to purchase " + quantity + " " + itemDesc;
+           String message2 = "Total cost with tax is: " + "$" + (price + tax) ;
+
         
         System.out.println(message);
+        System.out.println(message2);
         
         // Print another message with the total cost
         
