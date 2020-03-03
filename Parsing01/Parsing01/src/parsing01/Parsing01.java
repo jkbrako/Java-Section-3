5e8d3fc19605d3dc3886045f7c848904732ee9d1
Jamin Brako <jbrako@ucvts.tec.nj.us>
2020-03-03 13:11:12 -0500

    finished Parsing01

--- Parsing01/Parsing01/src/parsing01/Parsing01.java
+++ Parsing01/Parsing01/src/parsing01/Parsing01.java
@@ -3,13 +3,14 @@ package parsing01;
 public class Parsing01 {
     public static void main(String[] args) {
         //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
+        int shirtPrice = Integer.parseInt("15");
+        double taxRate = Double.parseDouble("0.05");
+        String gibberish = "887ds7nds87dsfs";
         
         
         
         //Parse shirtPrice and taxRate, and print the total tax
-        
-        
+        System.out.println(shirtPrice * taxRate);
         
         //Try to parse taxRate as an int
         
